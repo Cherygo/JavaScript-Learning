4b4ed6ebe989f442ac32dc7043d4c008463fdb5a
Cherygo <olexdzhy@gmail.com>
2025-10-07 17:45:17 +0300

    Completed Arrays task 2

---
+++ Arrays/task2.js
@@ -0,0 +1,5 @@
+// create a function that takes an array and returns only its last element
+function arrayLast(array = []){
+    return array[array.length - 1];
+}
+console.log(arrayLast([1,2,3,4,56,7,32, "asd"]));
