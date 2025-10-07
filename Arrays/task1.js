6547c5f211d4a7d84823b770ddba4053e7287ec2
Cherygo <olexdzhy@gmail.com>
2025-10-07 17:24:24 +0300

    Completed Arrays task 1

--- Arrays/task1.js
+++ Arrays/task1.js
@@ -1,1 +1,5 @@
-//
+//create a function that takes an array and returns its first element
+function arrayFirst(array = []){
+    return array[0];
+}
+console.log(arrayFirst(["alex", "stefan", "pesho"]))
