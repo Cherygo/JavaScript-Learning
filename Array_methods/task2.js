e4e43c265dc608afecf20c74cd1ed8ebc80fb548
Cherygo <olexdzhy@gmail.com>
2025-10-08 16:33:00 +0300

    Updated code to use BubbleSort method instead of array.sort()

--- Array_methods/task2.js
+++ Array_methods/task2.js
@@ -4,7 +4,22 @@
 
 // Examples
 // sortNumsAscending([1, 2, 10, 50, 5]) - [1, 2,5, 10, 50]
-function soryNumsAscending(arr = []) {
-    return arr.sort((num1, num2) => num1 - num2);
+function sortNumsAscending(arr = []) {
+    // return arr.sort((num1, num2) => num1 - num2);
+    let length = arr.length;
+    let swapped;
+    for(let i = 0; i < length; i++){
+        swapped = false;
+        for(let j = 0; j < length - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+        if(!swapped){
+            break;
+        }
+    }
+    return arr;
 }
-console.log(soryNumsAscending([1, 2, 10, 50 ,5]));
+console.log(sortNumsAscending([1, 2, 10, 50 ,5]));
