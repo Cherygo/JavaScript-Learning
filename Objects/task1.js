71663a6b068f3ab9bb73fed6efeff7e9c7cc14c0
Cherygo <olexdzhy@gmail.com>
2025-10-09 17:34:17 +0300

    Completed Objects task 1

---
+++ Objects/task1.js
@@ -0,0 +1,13 @@
+// Task 1
+// Create a function that takes an object argument sizes 
+// (contains width, length, height keys) and returns the volume of the box.
+
+// Examples volumeOfBox({ width: 2, length: 5, height: 1 }) - 10
+
+// Notes
+// . Remember that the values are in an object.
+// · Volume is length width height.
+function volumeOfBox(obj = {width, length, height}){
+    return obj.width * obj.height * obj.length;
+}
+console.log(volumeOfBox({width: 2, length: 5, height: 1}));
